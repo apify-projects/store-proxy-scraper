2fff6a921eae49e88cda5b42486db641e74c9247
Matt Stephens <87805115+mstephen19@users.noreply.github.com>
2022-10-27 10:18:00 +0200

    refactor

--- src/agents/proxy11.ts
+++ src/agents/proxy11.ts
@@ -13,22 +13,6 @@ const controller: Controller = ({ $ }) => {
             protocol: null,
         }
     );
-
-    return [...$('tbody tr')].map((item) => {
-        const elem = $(item);
-
-        const host = elem.find('td:first-child').text().trim();
-        const port = elem.find('td:nth-child(2)').text().trim();
-
-        return {
-            host,
-            port,
-            full: `${host}:${port}`,
-            anonymity: elem.find('td:nth-child(3)').text().toLowerCase(),
-            protocol: null,
-            country: elem.find('td:nth-child(4)').text().toLowerCase(),
-        };
-    });
 };
 
 export const proxy11One = generateAgent('https://proxy11.com/', controller);
--- src/agents/scrapingAnt.ts
+++ src/agents/scrapingAnt.ts
@@ -1,25 +1,36 @@
 import { generateAgent } from '../utils/generateAgent.js';
+import { proxyDataFromTable } from '../utils/proxyDataFromTable.js';
 
 export const scrapingAnt = generateAgent('https://scrapingant.com/proxies', ({ $ }) => {
-    const rows = $('tr:not(:first-child)').toArray();
+    return proxyDataFromTable(
+        { $, rows: 'tr:not(:first-child)' },
+        {
+            host: 1,
+            port: 2,
+            protocol: 3,
+            country: 4,
+        }
+    );
 
-    return rows.map((item) => {
-        const elem = $(item);
+    // const rows = $('tr:not(:first-child)').toArray();
 
-        const host = elem.find('td:first-child').text().trim();
-        const port = elem.find('td:nth-child(2)').text().trim();
+    // return rows.map((item) => {
+    //     const elem = $(item);
 
-        return {
-            host,
-            port,
-            full: `${host}:${port}`,
-            protocol: elem.find('td:nth-child(3)').text().trim().toLowerCase(),
-            country: elem
-                .find('td:nth-child(4)')
-                .text()
-                .replace(/[^\w\s]/g, '')
-                .trim(),
-            anonymity: null,
-        };
-    });
+    //     const host = elem.find('td:first-child').text().trim();
+    //     const port = elem.find('td:nth-child(2)').text().trim();
+
+    //     return {
+    //         host,
+    //         port,
+    //         full: `${host}:${port}`,
+    //         protocol: elem.find('td:nth-child(3)').text().trim().toLowerCase(),
+    //         country: elem
+    //             .find('td:nth-child(4)')
+    //             .text()
+    //             .replace(/[^\w\s]/g, '')
+    //             .trim(),
+    //         anonymity: null,
+    //     };
+    // });
 });
